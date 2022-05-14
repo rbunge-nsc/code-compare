a9dc239a69d2c1099c5ac860c2fd70ef51375bed
sbeduya <103277713+sbeduya@users.noreply.github.com>
2022-05-13 23:38:38 -0700

    Update DeliberateErrors.java
    After Linter check, revised w/ corrections.

--- DeliberateErrors.java
+++ DeliberateErrors.java
@@ -4,19 +4,27 @@
 class DeliberateErrors {
     main(String[] args) {
         //Removed "r" from print
-        System.out.pintln("Starting an error.\n");
+        //System.out.pintln("Starting an error.\n");
+        //Revised after Linter Check
+        System.out.println("Starting an error.\n");
 
         //Changed 2 to 2.0
-        int answer = 5 / 2.0;
+        //int answer = 5 / 2.0;
+        //Revised after Linter Check
+        int answer = 5 / 2;
         System.out.println(answer + "\n");
 
         //Need to end string with "
-        String errorHere = "Right here\n';
+        //String errorHere = "Right here\n';
+        //Revised after Linter Check
+        String errorHere = "Right here\n";
         System.out.println(errorHere);
 
         //Changed 1 to 1.0
         int integer;
-        integer = 1.0;
+        //integer = 1.0;
+        //Revised after Linter Check
+        integer = 1;
         System.out.println("Integer is " + integer);
     }
 }
